8b15fd985b7a9b222eaabd5f78e690b0fc9a6f1b
Grady Kuhnline <github@heygrady.net>
2012-03-20 17:47:29 -0700

    removing the watch method and leaving that declaration inline.

--- lib/compass/sprockets.rb
+++ lib/compass/sprockets.rb
@@ -24,34 +24,32 @@ module Compass
     end
     
     # register a custom watch
-    def watch
-      Compass.configuration.watch Compass.configuration.javascripts_src_dir + "/**/*" do |project_dir, relative_path|
-        puts project_dir
-        puts relative_path
-        if File.exists?(File.join(project_dir, relative_path))
-          # configure environment
-          environment = Sprockets::Environment.new(project_dir)
-          environment.append_path(Compass.configuration.javascripts_src_dir)
+    Compass.configuration.watch Compass.configuration.javascripts_src_dir + "/**/*" do |project_dir, relative_path|
+      puts project_dir
+      puts relative_path
+      if File.exists?(File.join(project_dir, relative_path))
+        # configure environment
+        environment = Sprockets::Environment.new(project_dir)
+        environment.append_path(Compass.configuration.javascripts_src_dir)
 
-          # add compressor if defined
-          if Compass.configuration.compress_javascript
-            environment.js_compressor = Sprockets::LazyCompressor.new {
-              Sprockets::Compressors.registered_js_compressor(Compass.configuration.javascript_compressor)
-            }
-          end
-          
-          # configure the static compiler
-          compiler = Sprockets::StaticCompiler.new(
-            environment,
-            Compass.configuration.javascripts_dir,
-            [/^([\w-]+\/)?[^_]\w+\.(js|coffee)$/],
-            :manifest_path => Compass.configuration.javascripts_dir,
-            :digest => false,
-            :manifest => false,
-            :zip_files => /a^/
-            )
-          compiler.compile
+        # add compressor if defined
+        if Compass.configuration.compress_javascript
+          environment.js_compressor = Sprockets::LazyCompressor.new {
+            Sprockets::Compressors.registered_js_compressor(Compass.configuration.javascript_compressor)
+          }
         end
+        
+        # configure the static compiler
+        compiler = Sprockets::StaticCompiler.new(
+          environment,
+          Compass.configuration.javascripts_dir,
+          [/^([\w-]+\/)?[^_]\w+\.(js|coffee)$/],
+          :manifest_path => Compass.configuration.javascripts_dir,
+          :digest => false,
+          :manifest => false,
+          :zip_files => /a^/
+          )
+        compiler.compile
       end
     end
   end
